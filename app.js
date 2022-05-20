77f2c48b9f7bc79417f991dcd97c0fec35818b2a
Denis Coelho <deniscoelho.dev@gmail.com>
2022-05-20 10:02:37 -0300

    atualizacao

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 const bookRouter = express.Router();
+const db = mongoose.connect("mongodb://localhost/bookAPI");
 const port = process.env.PORT || 3000;
+const Book = require("./models/bookModel");
 
-bookRouter.route("/books").get((req, res) => {
-  const response = { hello: "This is my API" };
-
-  res.json(response);
+bookRouter.route("/books/:bookId").get((req, res) => {
+  const { bookId } = req.params;
+  Book.findById(bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json(book);
+    }
+  });
 });
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my nodemon API");
