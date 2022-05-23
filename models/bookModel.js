a445f2e1c2b9984df74d6a5e38b0956dc10fb14c
Denis Coelho <deniscoelho.dev@gmail.com>
2022-05-22 22:00:05 -0300

    atualizacao

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const bookModel = Schema({
-  title: { type: String },
-  author: { type: String },
-  genre: { type: String },
-  read: { type: Boolean, default: false },
-});
+const bookModel = new Schema(
+  {
+    title: { type: String },
+    author: { type: String },
+    genre: { type: String },
+    read: { type: Boolean, default: false },
+  }
+);
 
-module.exports = mongoose.model("Book", bookModel);
+module.exports = mongoose.model('Book', bookModel);
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,30 +1,12 @@
 const express = require("express");
+const booksController = require("../controllers/booksController");
 
 function routes(Book) {
   const bookRouter = express.Router();
-  bookRouter
-    .route("/books")
-    .post((req, res) => {
-      const book = new Book(req.body);
-
-      book.save();
-      return res.status(201).json(book);
-    })
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-    });
-  bookRouter.use("/book/:bookId", (req, res, next) => {
-    const { bookId } = req.params;
-    Book.findById(bookId, (err, book) => {
+  const controller = booksController(Book);
+  bookRouter.route("/books").post(controller.post).get(controller.get);
+  bookRouter.use("/books/:bookId", (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
       }
@@ -37,17 +19,51 @@ function routes(Book) {
   });
   bookRouter
     .route("/books/:bookId")
-    .get((req, res) => res.json(req.book))
+    .get((req, res) => {
+      const returnBook = req.book.toJSON();
+      returnBook.links = {};
+      const genre = req.book.genre.replace(" ", "%20");
+      returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+      res.json(returnBook);
+    })
     .put((req, res) => {
       const { book } = req;
       book.title = req.body.title;
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
     });
-
   return bookRouter;
 }
 
